b320dc77f50a6f4ac7efd4089548a80a1c05a4d9
bklybor <bklybor@users.noreply.github.com>
2019-04-21 10:02:31 -0400

    something

--- NetworksFinal.py
+++ NetworksFinal.py
@@ -3,6 +3,7 @@ import uuid
 from pynput import keyboard
 import time
 from player import player
+from game import game
 
 UDP_IP = "172.25.39.37"
  
@@ -11,23 +12,29 @@ UDP.bind(('', 0));
 UDP.setblocking(0)
 
 # (ip_address, port_num, player_id, player_username)
-player1 = player("1.11", 2, "11.1", "shiji's a bitch")
+player1 = player("1.11", 1, "11.1", "shiji's a bitch")
 player2 = player("2.22", 2, "22.2", "fuck you shinji, you whingeing bastard")
 player_list = [player1, player2]    
 
+# (game_id, player1_id, player2_id)
+game1 = game(uuid.uuid4(), player1.id, player2.id)
+games_list = [game1]
 
-def add_player():
-    for address, port, id, name in player_list:
-        if address == addr:
+# add a new visitor to the server as a player
+def add_player(ip, port_num):
+    for addr, port, id, name in player_list:
+        if addr == ip:
             return 0
-    player_list.append((address,uuid.uuid4(),""))
+    player_list.append(player(ip,port_num,uuid.uuid4(),""))
+    return 1
 
 # send a username request message to the specified user
-def add_username(player_ip, player_port):
-    message = "Username Request"
+def add_username(player_ip, player_port, player_id):
+    message = "UR:{0}".format(player_id)
     smessage = str.encode(message)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(smessage, (player_ip, player_port))
+    sock.close()
 
 # update username for given user
 def receive_username(player_ip, player_username):
@@ -36,12 +43,13 @@ def receive_username(player_ip, player_username):
             player_list[index] = (ip, port, id, player_username)
             print(player_list[index])
 
-# send lobby information
+# send lobby information and direct to lobby
 def send_lob_info(player_ip, player_port):
-    message = "Current Lobby Size: {0}".format(len(player_list))
+    message = "TL:{0}".format(len(player_list))
     smessage = str.encode(message)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(smessage, (player_ip, player_port))
+    sock.close()
 
 # disconnect user (delete player from player list)
 def disconnect(user_id):
@@ -49,9 +57,77 @@ def disconnect(user_id):
         if p.id == user_id:
             player_list.remove(p)
 
-# retrieve information from port, send to correct game
-# format is "Update Game: game_id, player_id, move"
-def direct_traffic():
+# let the client know that the connectio has been rejected
+def reject_conn(ip, port, msg):
+    message = "RJ:{0}".format(msg)
+    smessage = str.encode(message)
+    sock = socket.socket(socket.AFT_INET, socket.SOCK_DGRAM)
+    sock.sendto(smessage, (ip, port))
+    sock.close()
+
+# let the client know that the connection has been accepted
+def accept_conn(ip, port):
+    message = "AC: "
+    smessage = str.encode(message)
+    sock = socket.socket(socket.AFT_INET, socket.SOCK_DGRAM)
+    sock. sendto(smessage, (ip, port))
+    sock.close()
+
+# general error message sender
+def send_error(ip,port,msg):
+    message = "ER:{0}".format(msg)
+    smessage = str.encode(message)
+    sock = socket.socket(socket.AFT_INET, socket.SOCK_DGRAM)
+    sock.sendto(smessage, (ip, port))
+    sock.close()
+
+# retrieve information from port, send to correct game, lobby, etc.
+# format is "{2 Character message type}:field1,field2,field3,...."
+def direct_traffic(message, ip, port):
+    msg_type = message[:2]
+    msg = message[3:]
+    if msg_type == "LR": # logon request
+        print(msg_type)
+        new_player = add_player(ip,port)
+        if not new_player:
+            reject_conn(ip, port, "Another player with the same ip has alread joined."
+                            "New connection denied.")
+        else:
+            accept_conn(ip,port)
+
+    elif msg_type == "SU": # send username
+        print(msg_type)
+        username, user_id = msg.split(',')
+        for p in player_list:
+            if p.id == user_id:
+                p.set_username(username)
+    elif msg_type == "SM": # send move
+        print(msg_type)
+        game_id, user_id, move = msg.split(',')
+        game_found = false
+        for game in games_list:
+            if game.id == game_id:
+                if move == "0":
+                    game.move_up(user_id)
+                elif move == "1":
+                    game.move_down(user_id)
+                else:
+                    send_error(ip,port,"Invalid move sent.")
+                game_found = true
+                break
+        if not game_found:
+            send_error(ip,port,"Game not found.")
+    elif msg_type == "QR": # quit or rematch
+        print(msg_type)
+        game_id, user_id, option = msg.split(',')
+    elif msg_type == "BM": # begin matchmaking
+        print(msg_type)
+        user_id = msg
+    elif msg_type == "DC": # disconnect
+        print(msg_type)
+        user_id = msg
+    else:
+        print(msg_type)
 
 
 if __name__ == "__main__":
--- game.py
+++ game.py
@@ -1,4 +1,22 @@
 class game(object):
-    def __init__(self, player1_id, player2_id):
+    def __init__(self, player1_id, player2_id, game_id):
         self.player1 = player1_id
         self.player = player2_id
+        self.id = game_id
+        self.player1_pos = 0
+        self.player2_pos = 0
+        self.ball_pos = 0
+        self.box_width = 400
+        self.box_height = 400
+
+    def move_up(player_id):
+        if player_id == player1:
+            player1_pos = player1_pos + 1
+        else:
+            player2_pos = player2_pos + 1
+
+    def move_down(player_id):
+        if player_id == player1:
+            player1_pos = player1_pos + 1
+        else:
+            player2_pos = player2_pos + 1
