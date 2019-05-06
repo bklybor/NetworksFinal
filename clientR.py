7e115b75fe99a48525d6b804782e7139e082544c
Nitulio <Njtulio8@gmail.com>
2019-05-05 20:50:11 -0400

    Add files via upload

--- clientR.py
+++ clientR.py
@@ -1,10 +1,7 @@
-import socket
-from pynput import keyboard
 from tkinter import *
+import socket
 import random
 import time
-
-
 # Define ball properties and functions
 class Ball:
     def __init__(self, canvas, color, size, paddle, paddle2):
@@ -16,60 +13,50 @@ class Ball:
         self.xspeed = 5
         self.yspeed = -1
 
-    def draw(self, x, y):
-        pos = self.canvas.coords(0, int(x), int(y))
+    def draw(self,x,y):
+        self.canvas.move(self.id, x, y)
 
-
-# Define paddle properties and functions
+# paddle 1 functions
 class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0,0, 10, 100, fill=color) # player 2, your opponent!
+        self.yspeed = 0
+        self.canvas.move(self.id, 480, 150)        
+    def draw(self,x):
+        self.canvas.move(self.id,0,x)
+class Paddle2:
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0,0, 10, 100, fill=color) 
         self.yspeed = 0
-        self.canvas.move(self.id, 10, 150 )
-
-
-    def draw(self, x):
-        y = int(x)
-        pos = self.canvas.coords(0, 480, y)
-        
-
-class Paddle2:
-    def __init__(self, canvas, color):
-       self.canvas = canvas
-       self.id = canvas.create_rectangle(0,0, 10, 100, fill=color) 
-       self.yspeed = 0
-       self.canvas.move(self.id, 480, 150)
-       self.canvas.bind_all('<KeyPress-Up>', self.move_up2)
-       self.canvas.bind_all('<KeyPress-Down>', self.move_down2)
-
-    def draw(self, x):
-        y = int(x)
-        print ("Converted")
-        pos = self.canvas.coords(0, 480, y)
-        print ("coords updated")
-
-    def move_up2(self, evt):
+        self.canvas.move(self.id, 10, 150)
+        self.canvas.bind_all('<KeyPress-Up>', self.move_up)
+        self.canvas.bind_all('<KeyPress-Down>', self.move_down)
+    def draw(self,x):
+        print(x)
+        self.canvas.move(self.id,0,x)
+    def move_up(self, evt):
         PORT = 5006
         IP = "172.25.40.161"
         MESSAGE = "SM:1,u"
-        print (MESSAGE)
         SMESSAGE = str.encode(MESSAGE)
+        print (MESSAGE)
         sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock1.bind(('',5007))
         sock1.sendto(SMESSAGE, (IP, PORT))
-           
-    def move_down2(self, evt):
+    def move_down(self, evt):
         PORT = 5006
         IP = "172.25.40.161"
         MESSAGE = "SM:1,d"
-        print (MESSAGE)
         SMESSAGE = str.encode(MESSAGE)
+        print (MESSAGE)
         sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock1.bind(('',5007))
         sock1.sendto(SMESSAGE, (IP, PORT))
 
-# Create window and canvas to draw on
+
+#Make background and declare colors and design
 tk = Tk()
 tk.title("Ping")
 canvas = Canvas(tk, width=500, height=400, bd=0, bg='white')
@@ -78,37 +65,54 @@ tk.update()
 paddle = Paddle(canvas, 'black')
 paddle2 = Paddle2(canvas, 'black')
 ball = Ball(canvas, 'black', 25, paddle, paddle2)
+
 UDP_IP = "0.0.0.0"
-UDP_PORT = 55433
- 
+UDP_PORT = 10500
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
+
 sock.setblocking(0)
-holder = ["1","150","150","250","200","0","0",0]
-isLoss = False
-toParse = ""
-# Animation loop
-while isLoss != True:
+is_loss = False
+datastring = ""
+toParse = ["1","0","0","250","200","0","0","0"]
+player1_speed = 0
+player1_pos = 150
+player2_speed = 0
+player2_pos = 150
+ball_x_pos = 250
+ball_x_speed = 0
+ball_y_pos = 200
+ball_y_speed = 0
+#Game Loop
+while is_loss == False:
     try:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         print ("received message:", data)
-        toParse = data.decode("utf-8")
-        print (toParse)
-        holder = toParse.split(",")
-        print (holder)
+        datastring = data.decode("utf-8")
+        toParse = datastring.split(",")
+
+        player1_speed = player1_pos - int(toParse[1])
+        player2_speed = player2_pos - int(toParse[2])
+        print("player2pos: ", player2_pos)
+        player1_pos = int(toParse[1])
+        player2_pos = int(toParse[2])
+        paddle.draw(player1_speed)
+        paddle2.draw(player2_speed)
+
+        ball_x_speed = ball_x_pos - int(toParse[3])
+        print("ballxspeed : ", ball_x_speed)
+        ball_y_speed = ball_y_pos - int(toParse[4])
+        print("ballyspeed : ", ball_y_speed)
+        ball_x_pos = int(toParse[3])
+        ball_y_pos = int(toParse[4])
+        ball.draw(toParse[3],toParse[4])
     except:
         pass
-    if holder[7] == "1":
-        isLoss = True
-    paddle.draw(holder[1])
-    paddle2.draw(holder[2])
-    ball.draw(holder[3],holder[4])
+    if toParse[7] == "1":
+        is_loss = True
     tk.update_idletasks()
     tk.update()
-    time.sleep(0.01)
-
-
-
-# Game Over
-go_label = canvas.create_text(250,200,text="GAME OVER",font=("Helvetica",30))
+    time.sleep(.01)
+#Game Over
+gameover_label = canvas.create_text(250,200,text="GAME OVER",font=("Calibri",30))
 tk.update()
